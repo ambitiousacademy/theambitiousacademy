f501ecf604397fe08ed4dad6526b2e6848a83ae3
iachintyatiwari <achintyatiwari0@gmail.com>
2024-07-12 23:54:42 +0530

    faculty pages completed

---
+++ src/app/faculty/aditya-trivedi/page.tsx
@@ -0,0 +1,108 @@
+const Faculty = () => {
+    return (
+      <div className="max-w-4xl mx-auto px-4 py-8 border-double border-4 rounded-2xl border-orange-600" >
+       
+        <h1 className="text-4xl font-bold mb-4">Aditya Trivedi</h1>
+        <section className="mb-6">
+       < img 
+          src='' 
+          alt="Faculty Image" 
+          className="object-cover h-40 w-50 rounded-lg"
+        />
+        </section>
+        <section className="mb-6" >
+          <ul className="list-disc list-inside">
+            <li className="mb-2">
+              <strong>Current Position: </strong>Advocate, Delhi High Court.</li>
+              </ul>
+            <li className="mb-2">
+              <strong>Education: </strong>
+              <ul className="list-decimal ml-20 list-outside" >
+                <li>
+              LL.M. in Competition Law and Economics (Ongoing) from Brussels School of Competition, Belgium.
+              </li>
+              <li>
+              B.A.LL.B. (Hons.) with specialization in Business Laws and Intellectual Property Rights from National Law University and Judicial Academy, Assam.
+              </li>
+                </ul>
+                </li>
+            <li className="mb-2">
+              <strong>Professional Experience: </strong>
+              <ul className="list-decimal ml-20 mb-4 list-outside" >
+                <li>
+                <strong>Associate at Competition Advisory Services (India) LLP: </strong>Worked with former Chairperson of Competition Commission of India, advising on competition law, regulatory compliance, and investment laws.</li>
+              <li>
+              <strong>Legal Counsel at Group Avenues: </strong>
+              Advised on contracts, intellectual property, and legal matters.</li>
+              <li>
+              <strong>Legal Consultant at Pyramids Legal LLP: </strong>Legal Consultant at Pyramids Legal LLP</li>
+                </ul>
+              </li>
+            <li className="mb-2">
+              <strong>Key Achievements: </strong>
+              <ul className="list-decimal ml-40 list-inside" >
+              <li className="mb-2">
+            Advised major conglomerates on competition law cases.
+              </li>
+              <li className="mb-2">
+              Prepared a competition compliance manual for Institute of Directors.
+              </li>
+              <li className="mb-2">
+              Advised on high-profile cases including an aviation merger and antitrust cases.
+              </li>
+              <li className="mb-2">
+              Published articles in national dailies and journals on competition law.
+              </li>
+              <li className="mb-2">
+              Participated and presented in international conferences on competition law
+              </li>
+                </ul>
+                </li>
+            <li className="mb-2">
+              <strong>Positions of Responsibility: </strong>
+              <ul className="list-decimal ml-40 list-inside" >
+              <li className="mb-2">
+            Advised major conglomerates on competition law cases.
+              </li>
+              <li className="mb-2">
+              Founder of "In Conversation with IPR and Competition Law" podcast.
+              </li>
+              <li className="mb-2">
+              Founding Director of Arth Vidhi and Atlas-Young Founders’ Club.
+              </li>
+              <li className="mb-2">
+              Non-Executive Director at Samacharline Media Group.
+              </li>
+              <li className="mb-2">
+              Associate Editor of The Commercial and Competition Law Review.
+              </li>
+              <li className="mb-2">
+              Policy Associate for Climate Change at Y20 India Secretariat, G-20 Engagement Group.
+              </li>
+                </ul>
+              </li> 
+            <li className="mb-2">
+              <strong>Professional Affiliations: </strong>
+              <ul className="list-decimal ml-40 list-inside" >
+              <li className="mb-2">
+              Member of Bar Council of Delhi.
+              </li>
+              <li className="mb-2">
+              	Member of Young Leaders Council at All India Management Association (AIMA).
+              </li>
+              <li className="mb-2">
+              	Ambassador for Kaavyavarsha.
+              </li>
+              <li className="mb-2">
+              Member of National Youth Council of India, Young India Foundation, and Advocates and Judges Cell at India Youth Club.
+              </li>
+              </ul>
+              </li> 
+              <p><strong>Aditya Trivedi is a distinguished advocate with extensive experience in competition law, intellectual property, and corporate law, known for his advisory roles, publications, and active participation in high-level legal conferences.
+                </strong></p>
+        </section>
+      </div>
+    );
+  };
+  
+  export default Faculty;  
---
+++ src/app/faculty/ankita-pandey/page.tsx
@@ -0,0 +1,88 @@
+const Faculty = () => {
+    return (
+      <div className="max-w-4xl mx-auto px-4 py-8 border-double border-4  rounded-2xl border-orange-600" >
+       
+        <h1 className="text-4xl font-bold mb-4">Ankita Pandey</h1>
+        <section className="mb-6">
+       < img 
+          src='' 
+          alt="Faculty Image" 
+          className="object-cover h-40 w-50 rounded-lg"
+        />
+        </section>
+        <section className="mb-6" >
+          <ul className="list-disc list-inside">
+            <li className="mb-2">
+              <strong>Professional Lawyer: </strong>Working at District & Sessions Court, Rewa, specializing in criminal, civil litigations, and legal compliances.</li>
+              </ul>
+            <li className="mb-2">
+              <strong>Key Legal Expertise: </strong>
+              <ul className="list-decimal ml-20 mb-4 list-outside" >
+                <li>
+                Drafting Search Reports for banks.
+                </li>
+              <li>
+              Drafting Notices under the Negotiable Instruments Act, 1881.
+              </li>
+              <li>
+              	Handling revenue, civil court, and RERA cases.
+              </li>
+                </ul>
+                </li>
+            <li className="mb-2">
+              <strong>Work Experience: </strong>
+              <ul className="list-decimal ml-20 mb-4 list-outside" >
+                <li>
+                <strong>2018-Present: </strong>
+                Managing cases related to revenue, civil court, and RERA.
+                </li>
+                <li>
+                <strong>2023-2024: </strong>
+                Assistance Professor at Indore Institute of Law.
+              </li>
+              <li>
+                <strong>2017-2018: </strong>
+                Drafted legal notices and various deeds as Legal Apprentice.
+              </li>
+              <li>
+              Freelance writer with strong research and writing skills and a content creator
+              </li>
+              </ul>
+              </li>
+            <li className="mb-2">
+              <strong>Educational Background: </strong>
+              <ul className="list-decimal ml-16 list-inside" >
+              <li className="mb-2">
+              BA LLB (Hons) from National Law University & Judicial Academy, Assam.
+              </li>
+              <li className="mb-2">
+                	LLM (Corporate and Commercial Law) 
+              </li>
+                </ul>
+                </li>
+            <li className="mb-2">
+              <strong>Research and Writing Skills: </strong>
+              <ul className="list-decimal ml-20 mb-4 list-outside" >
+              <li className="mb-2">
+              	Published articles on legal issues in renowned journals.
+              </li>
+              <li className="mb-2">
+              Strong experience in drafting various legal documents.
+              </li>
+                </ul>
+              </li> 
+            <li className="mb-2">
+              <strong>Extra-Curricular Activities: </strong>
+              <ul className="list-decimal ml-20 mb-4 list-outside" >
+              <li className="mb-2">
+              Worked with multiple NGOs.
+              </li>
+              </ul>
+              </li> 
+              <p><strong>Ankita Pandey is a dedicated lawyer with a robust academic background and extensive experience in legal drafting, research, and litigation.</strong></p>
+        </section>
+      </div>
+    );
+  };
+  
+  export default Faculty;  
